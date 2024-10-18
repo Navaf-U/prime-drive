12b5c6b44304132dd596f43b57953ddd158e043e
navaf <navafsoft@gmail.com>
2024-10-18 19:25:06 +0530

    toggleNavbar

--- app/src/Components/Navbar.jsx
+++ app/src/Components/Navbar.jsx
@@ -1,23 +1,26 @@
 import { CiLocationOn } from "react-icons/ci";
 import { HiOutlineUserCircle } from "react-icons/hi2";
 import { CiMenuBurger } from "react-icons/ci";
+import { CiSearch } from "react-icons/ci";
 import CarLogo from "../assets/LogoCar.png";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
 function Navbar() {
-  const [menuBar,setMenBar]=useState(true)
-  
+  const [menuBar,setMenBar]=useState(false)
+  const toggleBar=()=>{
+    setMenBar(!menuBar)
+  }
   return (
     <div className=" position fixed top-0 bg-white w-full z-50">
-      <div className="flex flex-col md:flex-row justify-around border-b-[1px] h-auto items-center border-[gray] p-4">
+      <div className="flex flex-row  justify-around border-b-[1px] h-auto items-center border-[gray] p-4">
         <NavLink to="/">
           
-          <div className="text-xl flex font-bold text-[#522258] justify-center items-center me-20">
+          <div className="text-xl  flex font-bold text-[#522258] justify-center items-center me-20">
             <img src={CarLogo} className="w-16 rounded-xl me-2" alt="" />
             PrimeDrive
           </div>
         </NavLink>
-        <div className="flex gap-5">
+        <div className="hidden md:flex gap-5">
           <NavLink to="/">
             
             <h1 className="cursor-pointer text-[#8C3061] hover:text-[#da45ee]">
@@ -33,14 +36,16 @@ function Navbar() {
             REVIEWS & NEWS
           </h1>
         </div>
-        <div>
+        <div className="hidden sm:flex w-[300px] me-5">
           <input
             type="text"
             placeholder="Search"
-            className="p-2 border w-[150%] border-gray-300 rounded-md outline-[#8C3061]"
+            className="p-2 border w-[100%] border-gray-300 rounded-md outline-[#8C3061]"
           />
         </div>
-        <div className="flex space-x-4">
+
+        <div className="flex gap-5">
+          <CiSearch size={30} className="text-[#8C3061 sm:hidden "/>
           <p className="text-[#8C3061] cursor-pointer hover:text-[#da45ee]">
             <CiLocationOn size={30} />
           </p>
@@ -48,7 +53,29 @@ function Navbar() {
             <HiOutlineUserCircle size={30} />
           </p>
         </div>
+      <CiMenuBurger className="cursor-pointer ms-5 md:hidden" onClick={toggleBar} />
       </div>
+
+
+      <div className={`fixed top-0 right-0 bg-[gray] text-white w-36 h-screen transition-transform duration-300 ease-in-out ${
+          menuBar ? "translate-x-0" : "translate-x-full"
+        } md:hidden z-50`}>
+          <NavLink to="/">
+            
+            <h1 onClick={toggleBar} className="cursor-pointer text-[#8C3061] hover:text-[#da45ee]">
+              NEW CARS
+            </h1>
+          </NavLink>
+          <NavLink to="/usedcars">
+            <h1 onClick={toggleBar} className="cursor-pointer text-[#8C3061] hover:text-[#da45ee]">
+              USED CARS
+            </h1>
+          </NavLink>
+          <h1  onClick={toggleBar} className="cursor-pointer text-[#8C3061] hover:text-[#da45ee]">
+            REVIEWS & NEWS
+          </h1>
+        </div>
+
     </div>
   );
 }
