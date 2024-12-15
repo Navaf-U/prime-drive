b509b01119b30a52dd0fccd8d820cfc8c609ae82
navaf <navafsoft@gmail.com>
2024-12-15 09:52:41 +0530

    feat (connectDB) : added a connection to database

---
+++ server/config/connectDB.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const ConnectDB = async () => {
+  mongoose.connection.on("connected", () => {
+    console.log("DB CONNECTED");
+  });
+  // represents the state
+   try{
+    await mongoose.connect(`${process.env.HOST}/database`   );
+   }catch(err){
+    console.log(err.message)
+   }
+  };
+
+  export default ConnectDB
