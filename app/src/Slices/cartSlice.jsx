021b08f6440e1b553d7fe66de2b7d680285d18b5
navaf <navafsoft@gmail.com>
2025-04-02 09:22:29 +0530

    refactor (cartSlice, productsSlice): improve code formatting and structure for better readability; update mongoose version in package files

--- app/src/Slices/cartSlice.jsx
+++ app/src/Slices/cartSlice.jsx
@@ -1,26 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
-
-const initialState = []
+const initialState = [];
 const cartSlice = createSlice({
-    name:"cart",
-    initialState,
-    reducers:{
-        add : (state,action)=>{
-        const exitingItem = state.find(item => item.id === action.payload.id);
-        if(exitingItem){
-            exitingItem.quantity += 1
-            exitingItem.totalPrice += action.payload.rate
-        }else{
-                state.push({...action.payload,quantity : 1,totalPrice: action.payload.rate })
-        }
+  name: "cart",
+  initialState,
+  reducers: {
+    add: (state, action) => {
+      const exitingItem = state.find((item) => item.id === action.payload.id);
+      if (exitingItem) {
+        exitingItem.quantity += 1;
+        exitingItem.totalPrice += action.payload.rate;
+      } else {
+        state.push({
+          ...action.payload,
+          quantity: 1,
+          totalPrice: action.payload.rate,
+        });
+      }
     },
-        removeCart: (state,action)=>{
-         return state.filter((item) => item.id !== action.payload )
-        }
-},
-})
+    removeCart: (state, action) => {
+      return state.filter((item) => item.id !== action.payload);
+    },
+  },
+});
 
-
-export const {add,removeCart} = cartSlice.actions;
+export const { add, removeCart } = cartSlice.actions;
 export default cartSlice.reducer;
--- app/src/Slices/productsSlice.jsx
+++ app/src/Slices/productsSlice.jsx
@@ -1,65 +1,69 @@
-import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-    products:[],
-    premiumProducts:[],
-    filteredProducts:[],
-    loading:false,
-    error:null,
+  products: [],
+  premiumProducts: [],
+  filteredProducts: [],
+  loading: false,
+  error: null,
 };
 
-export const fetchProducts = createAsyncThunk("products/fetch",async()=>{
-    const response = await axios.get("http://localhost:3000/public/products");
-    return response.data
-})
+export const fetchProducts = createAsyncThunk("products/fetch", async () => {
+  const response = await axios.get("http://localhost:3000/public/products");
+  return response.data;
+});
 
-export const fetchPremiumProducts = createAsyncThunk("premiumProducts/fetch",async()=>{
+export const fetchPremiumProducts = createAsyncThunk(
+  "premiumProducts/fetch",
+  async () => {
     const response = await axios.get("http://localhost:3000/premiumCars");
-    return response.data
-})
+    return response.data;
+  }
+);
 
 const productSlice = createSlice({
-    name:"products",
-    initialState,
-    reducers:{
-        filteredProducts(state,action){
-            const query = action.payload.toLowerCase();
-            state.filteredProducts = state.products.filter((item)=>
-            item.name.toLowerCase().includes(query) ||
-            item.company.toLowerCase().includes(query)||
-            item.model.toLowerCase().includes(query)
-            )
-        }
+  name: "products",
+  initialState,
+  reducers: {
+    filteredProducts(state, action) {
+      const query = action.payload.toLowerCase();
+      state.filteredProducts = state.products.filter(
+        (item) =>
+          item.name.toLowerCase().includes(query) ||
+          item.company.toLowerCase().includes(query) ||
+          item.model.toLowerCase().includes(query)
+      );
     },
-    extraReducers:(builder)=>{
-        builder
-        .addCase(fetchProducts.fulfilled,(state,action)=>{
-            state.loading = false
-            state.products = action.payload
-        })
-        .addCase(fetchProducts.rejected,(state,action)=>{
-            state.loading = false
-            state.error = action.error.message
-        })
-        .addCase(fetchProducts.pending,(state)=>{
-            state.loading = true
-            state.error=null
-        })
-        .addCase(fetchPremiumProducts.fulfilled,(state,action)=>{
-            state.loading = false
-            state.premiumProducts = action.payload
-        })
-        .addCase(fetchPremiumProducts.rejected,(state,action)=>{
-            state.loading = false
-            state.error = action.error.message
-        })
-        .addCase(fetchPremiumProducts.pending,(state)=>{
-            state.loading = true
-            state.error=null
-        })
-    }
-})
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products = action.payload;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      .addCase(fetchProducts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchPremiumProducts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.premiumProducts = action.payload;
+      })
+      .addCase(fetchPremiumProducts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      .addCase(fetchPremiumProducts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      });
+  },
+});
 
-export const {filteredProducts} = productSlice.actions
+export const { filteredProducts } = productSlice.actions;
 export default productSlice.reducer;
